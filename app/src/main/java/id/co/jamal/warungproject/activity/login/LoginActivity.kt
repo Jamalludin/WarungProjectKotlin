d50c4532bba2f874f21cc182b44cbfd2b457d6c6
Jamalludin <jamalludin1209@gmail.com>
2018-09-26 15:16:50 +0700

    add navigation drawer

--- app/src/main/java/id/co/jamal/warungproject/activity/login/LoginActivity.kt
+++ app/src/main/java/id/co/jamal/warungproject/activity/login/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import id.co.jamal.warungproject.R
+import id.co.jamal.warungproject.activity.HomeActivity
 import id.co.jamal.warungproject.activity.register.RegisterActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -15,11 +16,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         btnLogin.setOnClickListener {
-            Toast.makeText(this, "Hi there! This is a Toast.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
         }
 
         txtSigup.setOnClickListener {
-            val intent = Intent(applicationContext,RegisterActivity::class.java)
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent) }
     }
 }
